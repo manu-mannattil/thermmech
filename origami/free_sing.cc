95d8c9f6ccd3640241fd8c34fa34be4d59d6ab57
Manu Mannattil <manu.mannattil@posteo.net>
2021-10-22 02:40:52 -0400

    Estimate error in MC integration.

--- origami/free_sing.cc
+++ origami/free_sing.cc
@@ -15,7 +15,8 @@
 
 using namespace std;
 
-double U(const double& beta, const double& t, const valarray<double>& x) {
+// Integral over q4-q7.
+double U1(const double& beta, const double& t, const valarray<double>& x) {
     double u1 = 10 * t * t + 31 * x[0] * x[0] + 18 * x[0] * (2 * t - x[1]) - 12 * t * x[1] +
                 3 * x[1] * x[1];
     double u2 = 5 * x[0] * x[0] + 6 * x[0] * (2 * t - x[1]) + (x[1] - 2 * t) * (x[1] - 2 * t);
@@ -23,23 +24,41 @@ double U(const double& beta, const double& t, const valarray<double>& x) {
     return exp(-0.5 * beta * (u1 * u1 / 888 + u2 * u2 / 344));
 }
 
-double free(const double& beta, const double& t, const double& a = 10, const size_t& N = 1000) {
-    double I = 0;
-    for (size_t i = 0; i < N; i++)
-        I += U(beta, t, uranarray(2, -a, a));
-    I *= 4 * a * a / N;
-    return -log(I);
+// Integral over q7-q10.
+double U2(const double& beta, const double& t, const valarray<double>& x) {
+    double u1 = t * t - 12 * x[0] * x[0] - 20 * t * x[1] - 124 * x[1] * x[1] + 12 * x[0] * (t + 6 * x[1]);
+    double u2 = -3 * t * t + 4 * x[0] * x[0] + 28 * t * x[1] + 20 * x[1] * x[1] - 4 * x[0] * (t + 6 * x[1]);
+    return exp(-0.5 * beta * (u1 * u1 / 14208 + u2 * u2 / 5504));
+}
+
+valarray<double> free(const double& beta, const double& t, const double& a = 10,
+                      const long& N = 1000) {
+    double I = 0, I2 = 0;
+    double p, err;
+    for (auto i = 0; i < N; i++) {
+        p = U2(beta, t, uranarray(2, -a, a));
+        I += p;
+        I2 += p * p;
+    }
+
+    I = I / N;
+    I2 = I2 / N;
+    err = 4 * a * a * sqrt(I2 - I * I) / sqrt(N);
+    I *= 4 * a * a;
+
+    return {t * 180 / M_PI, -log(I), err / I, I, err};
 }
 
 int main(int argc, const char* argv[]) {
-    size_t N = 1000 * 1000 * 1000;
+    long N = 1000 * 1000;
+    N = N * N;
     double a = 10;
     double beta = 10 * 1000;
-    valarray<double> f(75 + 1);
+    vector<valarray<double>> res(75 + 1);
 
 #pragma omp parallel for
     for (auto t = 0; t < 75 + 1; t += 1)
-        f[t] = free(beta, t * M_PI / 180, a, N);
+        res[t] = free(beta, t * M_PI / 180, a, N);
 
     ofstream output;
     if (argc > 1)
@@ -47,8 +66,8 @@ int main(int argc, const char* argv[]) {
     else
         output.open("free_sing_mc.dat");
 
-    for (auto t = 0; t < 75 + 1; t += 1)
-        output << t << "\t" << f[t] << endl;
+    for (auto r : res)
+        output << r[0] << "\t" << r[1] << "\t" << r[2] << "\t" << r[3] << "\t" << r[4] << endl;
 
     return 0;
 }
